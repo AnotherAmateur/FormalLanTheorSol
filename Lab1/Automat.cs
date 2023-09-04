b8966ca31e4412c9ddbf2b478d865c215fff0a1d
Dmitrii <89616506285@ya.ru>
2023-09-04 18:29:12 +0400

    lab 1 is done

--- Lab1/Automat.cs
+++ Lab1/Automat.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FormalLanTheor
 {
     public class Automat
     {
+        const string PassSymb = "-";
+
         string? initState;
         List<string> finalStates;
-        List<char> alphabet;
+        List<char> alphabet;       
         Dictionary<string, Dictionary<char, string>> transMatrix;
-
+     
 
         public Automat(string path)
         {
@@ -33,6 +31,60 @@ namespace FormalLanTheor
         }
 
 
+        public List<string> Exec(string word)
+        {
+            List<string> logs = new();
+            string curState = initState;
+
+            foreach (char letter in word)
+            {      
+                string nextState = transMatrix[curState][letter];
+                logs.Add($"{letter}: {curState} -> {nextState}");
+
+                if (nextState.Equals(PassSymb))
+                {
+                    logs.Add("The final state wasn`t reached");
+                    break;
+                }
+
+                if (finalStates.Contains(nextState))
+                {
+                    logs.Add("The final state was reached");
+                    break;
+                }
+                         
+                curState = nextState;
+            }
+
+            if (word.Length == 0)
+            {
+                logs.Add("Empty input");
+                logs.Add("The final state wasn`t reached");
+            }
+
+            return logs;
+        }
+
+        public void PrintConfigFile()
+        {
+            Console.WriteLine();
+
+            Console.WriteLine($"Alphabet: {string.Join(", ", alphabet)}");
+            Console.WriteLine($"States: {string.Join(", ", transMatrix.Keys)}");
+            Console.WriteLine($"Initial state: {initState}");
+            Console.WriteLine($"Final state(s): {string.Join(", ", finalStates)}");
+
+            Console.WriteLine("Transition matrix:");
+            Console.WriteLine($"{new string(' ', 3)} {string.Join("  ", alphabet)}");
+            foreach (var line in transMatrix)
+            {
+                Console.WriteLine($"{line.Key} | {string.Join("  ", line.Value.Values)}");
+            }
+
+            Console.WriteLine();
+        }
+
+
         private void ReadFileConfig(string path)
         {
             using (var configFyle = new StreamReader(path, Encoding.UTF8))
@@ -44,6 +96,7 @@ namespace FormalLanTheor
                     throw new Exception();
                 }
 
+                // чтение алфавита
                 foreach (string letter in line.Split('/', StringSplitOptions.RemoveEmptyEntries))
                 {
                     if (letter.Length != 1)
@@ -55,6 +108,7 @@ namespace FormalLanTheor
                     alphabet.Add(char.Parse(letter));
                 }
 
+                // чтение числа состояний
                 int stateCount;
                 if (int.TryParse(configFyle.ReadLine(), out stateCount) is false)
                 {
@@ -62,6 +116,7 @@ namespace FormalLanTheor
                     throw new Exception();
                 }
 
+                // чтение переходов состояний
                 for (int i = 0; i < stateCount; ++i)
                 {
                     line = configFyle.ReadLine();
@@ -74,21 +129,47 @@ namespace FormalLanTheor
                     string[] transitions = line.Split('/', StringSplitOptions.RemoveEmptyEntries);
                     transMatrix.Add(transitions[0], new());
 
+                    if (transitions.Length - 1 != alphabet.Count)
+                    {
+                        // todo
+                        throw new Exception();
+                    }
+
                     for (int j = 1; j < transitions.Length; ++j)
                     {
-                        if (letter.Length != 1)
-                        {
-
-                        }
-
-                        alphabet.Add(char.Parse(letter));
+                        transMatrix[transitions[0]].Add(alphabet[j - 1], transitions[j]);
                     }
                 }
 
+                if (transMatrix.Count == 0)
+                {
+                    // todo
+                    throw new Exception();
+                }
 
+                // установка начального состояния
+                initState = configFyle.ReadLine();
+                if (initState is null || transMatrix.ContainsKey(initState) is false)
+                {
+                    // todo
+                    throw new Exception();
+                }
 
+                // установка конечных состояний
+                string? fStatesLine = configFyle.ReadLine();
+                if (fStatesLine is null)
+                {
+                    // todo
+                    throw new Exception();
+                }
+
+                finalStates = fStatesLine.Split('/', StringSplitOptions.RemoveEmptyEntries).ToList();
+                if (finalStates.All(transMatrix.ContainsKey) is false)
+                {
+                    // todo
+                    throw new Exception();
+                }
             }
         }
-
     }
 }
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,1 +1,62 @@
-﻿
+﻿namespace FormalLanTheor
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string configFilePath = "D:\\Uni\\4course_1sem\\FormalLanTheorSol\\Lab1\\automaton.me";
+
+            var automaton = new Automat(configFilePath);
+
+            while (true)
+            {
+                WelcomeMenu();
+
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice) is false)
+                {
+                    Console.WriteLine("Bad input");
+                    return;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        automaton.PrintConfigFile();
+                        break;
+                    case 2:
+                        {
+                            Console.Write("Word: _\b");
+                            string? word = Console.ReadLine();
+
+                            if (word is null)
+                            {
+
+                                break;
+                            }
+
+                            List<string> resultLogs = automaton.Exec(word);
+
+                            Console.WriteLine("------------------------------");
+                            foreach (var record in resultLogs)
+                            {
+                                Console.WriteLine(record);
+                            }
+                            Console.WriteLine("------------------------------");
+                        }
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+
+        private static void WelcomeMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press 1 to see the automaton info");
+            Console.WriteLine("Press 2 to enter a word");
+        }
+    }
+
+}
