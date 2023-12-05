c8705cdf35743c52d8bda4ade76d99341f527710
Dmitrii <dmitrii.dolgireff@ya.ru>
2023-12-06 02:10:46 +0400

    Add lab 6 - Syntax Analyzer

---
+++ Lab6_Syntax_Analyzer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Lab5_Lexical_Analyzer;
+
+namespace Lab6_Syntax_Analyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = @"for i = 1 + 2 to i + 23
+                                a = a - 1 + 5
+                                b = 56
+                            next";
+
+            (bool, List<Lexeme>) resLexemes = LexAnalyzer.Analyze(code);
+
+            for (int i = 0; i < resLexemes.Item2.Count; i++)
+            {
+                Console.WriteLine($"{i}. Category: {resLexemes.Item2[i].LexCat}, Type: {resLexemes.Item2[i].LexType}, " +
+                    $"Value: {resLexemes.Item2[i].Value}");
+            }
+            Console.WriteLine(resLexemes.Item1 ? "Success" : "Fail");
+            Console.WriteLine();
+
+            string result = SyntaxAnalyzer.Parse(resLexemes.Item2);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Lab6_Syntax_Analyzer/SyntaxAnalyzer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab5_Lexical_Analyzer;
+using Lab5_Lexical_Analyzer.Enums;
+
+namespace Lab6_Syntax_Analyzer
+{
+    public static class SyntaxAnalyzer
+    {
+        private static List<Lexeme> _lexemes;
+        private static int currentPos;
+        private const string FOR = "for";
+        private const string TO = "to";
+        private const string NEXT = "next";
+        private const string ASSIGNMENT = "=";
+
+        public static string Parse(List<Lexeme> lexemes)
+        {
+            _lexemes = lexemes;
+            currentPos = 0;
+
+            try
+            {
+                ParseForLoop();
+                return "Синтаксический анализ завершен успешно.";
+            }
+            catch (Exception ex)
+            {
+                return $"Ошибка в синтаксическом анализе: {ex.Message}";
+            }
+        }
+
+        private static Lexeme GetLexeme()
+        {
+            if (currentPos == _lexemes.Count)
+            {
+                ThrowParseException("Отсутствует следующея необходимая лексема", _lexemes[currentPos]);
+            }
+
+            return _lexemes[currentPos++];
+        }
+
+        private static Lexeme PeekLexeme()
+        {
+            if (currentPos == _lexemes.Count)
+            {
+                ThrowParseException("Отсутствует следующея необходимая лексема", _lexemes[currentPos]);
+            }
+
+            return _lexemes[currentPos];
+        }
+
+        private static void ParseForLoop()
+        {
+            CheckExpectation(FOR, Categories.Keyword);
+            ParseIdentifier();
+
+            CheckExpectation(ASSIGNMENT, Categories.SpecSymb);
+            ParseArithmeticExpression();
+
+            CheckExpectation(TO, Categories.Keyword);
+            ParseArithmeticExpression();
+
+            ParseOperators();
+            CheckExpectation(NEXT, Categories.Keyword);
+        }
+
+        private static void ParseArithmeticExpression()
+        {
+            ParseOperand();
+
+            Lexeme nextLexeme = PeekLexeme();
+
+            if (nextLexeme.LexType.Equals(LexTypes.Arithmetic))
+            {
+                GetLexeme();
+                ParseArithmeticExpression();
+            }
+        }
+
+        private static void ParseOperand()
+        {
+            Lexeme operand = GetLexeme();
+
+            if ((operand.LexCat.Equals(Categories.Identifier) || operand.LexCat.Equals(Categories.Const)) is false)
+            {
+                ThrowParseException("Ожидается операнд (идентификатор или константа).", operand);
+            }
+        }
+
+        private static void ParseIdentifier()
+        {
+            Lexeme lexeme = GetLexeme();
+
+            if (lexeme.LexCat.Equals(Categories.Identifier) is false)
+            {
+                ThrowParseException("Ожидается идентификатор.", lexeme);
+            }
+        }
+
+        private static void ParseOperators()
+        {
+            while (PeekLexeme().LexCat.Equals(Categories.Keyword) is false)
+            {
+                ParseIdentifier();
+                CheckExpectation(ASSIGNMENT, Categories.SpecSymb);
+                ParseArithmeticExpression();
+            }
+        }
+
+        private static void CheckExpectation(string expectedKeyword, Categories category)
+        {
+            Lexeme lexeme = GetLexeme();
+
+            if ((category.Equals(lexeme.LexCat) && lexeme.Value.Equals(expectedKeyword, StringComparison.OrdinalIgnoreCase)) is false)
+            {
+                ThrowParseException($"Ожидается ключевое слово '{expectedKeyword}'.", lexeme);
+            }
+        }
+
+        private static void ThrowParseException(string message, Lexeme lexeme)
+        {
+            throw new Exception($"Позиция: {currentPos}. {message} Найдено: {lexeme.LexType}," +
+                $" Категория: {lexeme.LexCat}, Значение: {lexeme.Value}");
+        }
+    }
+}
